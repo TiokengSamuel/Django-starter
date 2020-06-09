20a558c7163deba635ac9f92c19f8bd2876a92d6
SamuelEdward <samueledward40@gmail.com>
2020-06-09 04:21:21 -0700

    answer moving to the other page but is as a string

--- telusko/calc/views.py
+++ telusko/calc/views.py
@@ -7,4 +7,9 @@ def home(request):
     return render(request, 'home.html', {'name': 'samuel'})
 
 def add(request):
-    return render(request, 'result.html')
+
+    val1 = request.GET['num1']
+    val2 = request.GET['num2']
+    res = val1 + val2
+
+    return render(request, 'result.html', {'result':res})
