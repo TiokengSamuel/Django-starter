433fdb95a27d85e9b223d2d169e823bb82975462
SamuelEdward <samueledward40@gmail.com>
2020-06-11 09:50:39 -0700

    correcting error

--- telusko/telusko/settings.py
+++ telusko/telusko/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'telusko/static')
+    os.path.join(BASE_DIR, 'static')
 ]
-STACTIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
