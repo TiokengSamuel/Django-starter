bcd6d1fb0a122b75263a939077721fa993274b03
SamuelEdward <samueledward40@gmail.com>
2020-06-11 11:36:20 -0700

    creating an object destination

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {'price' : 700})
+
+    dest1 = Destination()
+
+    return render(request, 'index.html', {'dest1' : dest1})
