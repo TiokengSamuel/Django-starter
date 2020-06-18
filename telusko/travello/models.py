12d0ebc6b08575f032f04007000888fd7ace0564
SamuelEdward <samueledward40@gmail.com>
2020-06-18 04:16:19 -0700

    changing models for database use

--- telusko/travello/models.py
+++ telusko/travello/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Destination(models.Model):
-    id : int
-    name : str
-    desc : str
-    price : int
-    offer : bool
+    id = models.ImageField(upload_to='pics')
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.IntegerField
+    offer = models.BooleanField(default=False)
