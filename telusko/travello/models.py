c1872cc3c860c43924b457d1723ce96891dfaf85
SamuelEdward <samueledward40@gmail.com>
2020-06-18 04:26:01 -0700

    removing id model sinc primary key automatic

--- telusko/travello/models.py
+++ telusko/travello/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Destination(models.Model):
-    id = models.ImageField(upload_to='pics')
+    
+    img = models.ImageField(upload_to='pics')
     name = models.CharField(max_length=100)
     desc = models.TextField()
     price = models.IntegerField
