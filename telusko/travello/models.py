a5e6592bc69e8dccaa43eb6bca10594a42db5e65
SamuelEdward <samueledward40@gmail.com>
2020-06-19 09:37:45 -0700

    correcting () in models

--- telusko/travello/models.py
+++ telusko/travello/models.py
@@ -7,5 +7,5 @@ class Destination(models.Model):
     img = models.ImageField(upload_to='pics')
     name = models.CharField(max_length=100)
     desc = models.TextField()
-    price = models.IntegerField
+    price = models.IntegerField()
     offer = models.BooleanField(default=False)
