ea66dbc1cec07540523034d444168097ddcfffc8
SamuelEdward <samueledward40@gmail.com>
2020-06-25 05:35:14 -0700

    username

--- telusko/accounts/views.py
+++ telusko/accounts/views.py
@@ -8,5 +8,7 @@ def register(request):
     if request.method == 'POST':
         firstname = request.POST['first_name']
         lastname = request.POST['last_name']
+        username = request.POST['username']
+
 
     return render(request, 'register.html')
